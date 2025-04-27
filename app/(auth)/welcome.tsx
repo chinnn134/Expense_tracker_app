433c791c0bcdb16158c468749ce0109672cd555a
trinhtnd <trinhlly1994@gmail.com>
2025-04-27 15:12:20 +0700

    Nội dung cập nhật

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
@@ -27,39 +26,12 @@ const Welcome = () => {
             source={require("../../assets/images/welcome.png")}
             style={styles.welcomeImage}
             resizeMode="contain"
-=======
-import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import ScreenWrapper from '@/components/ScreenWrapper'
-import Typo from '@/components/Typo';
-import { colors, spacingX, spacingY } from '@/constants/theme'
-import { verticalScale } from '@/utils/styling'
-import Button from '@/components/Button'
-import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
-
-const welcome = () => {
-  return (
-    <ScreenWrapper>
-      <View style={styles.container}>
-        {/*Login button & image*/}
-        <View>
-          <TouchableOpacity style={styles.loginButton}>
-            <Typo fontWeight={"500"}>Sign in</Typo>
-          </TouchableOpacity>
-
-          <Animated.Image
-          entering={FadeIn.duration(1000)}
-          source={require("../../assets/images/welcome.png")}
-          style={styles.welcomeImage}
-          resizeMode="contain"
->>>>>>> 6163a122ef3268511a8c254e7e4007d395cfd283
           />
         </View>
         {/* footer */}
         <View style={styles.footer}>
           <Animated.View
             entering={FadeInDown.duration(1000).springify().damping(12)}
-<<<<<<< HEAD
             style={{ alignItems: "center" }}
           >
             <Typo size={30} fontWeight={"800"}>
@@ -69,20 +41,10 @@ const welcome = () => {
             <Typo size={30} fontWeight={"800"}>
               {" "}
               of your control
-=======
-            style={{ alignItems: "center"}}
-            >
-            <Typo size={30} fontWeight={"800"}>
-              Always take control
-            </Typo>
-            <Typo size={30} fontWeight={"800"}>
-              of your finances
->>>>>>> 6163a122ef3268511a8c254e7e4007d395cfd283
             </Typo>
           </Animated.View>
 
           <Animated.View
-<<<<<<< HEAD
             entering={FadeInDown.duration(1000)
               .delay(100)
               .springify()
@@ -94,7 +56,7 @@ const welcome = () => {
               Finances must be arranged to set a better
             </Typo>
             <Typo size={17} color={colors.textLight}>
-              {" "}
+              {/* {" "} */}
               lifestyle in future
             </Typo>
           </Animated.View>
@@ -108,24 +70,6 @@ const welcome = () => {
             {/* button */}
             <Button onPress={() => router.push("/(auth)/register")}>
               <Typo size={22} color={colors.neutral900} fontWeight={"600"}>
-                {" "}
-=======
-            entering={FadeInDown.duration(1000).delay(100).springify().damping(12)}
-             style={{ alignItems: "center", gap: 2}}>
-            <Typo size={17} color={colors.textLight}>
-              Finances must be arranged to set a better
-            </Typo>
-            <Typo size={17} color={colors.textLight}>
-              lifestyle in future
-            </Typo>
-          </Animated.View>
-
-          <Animated.View
-            entering={FadeInDown.duration(1000).delay(200).springify().damping(12)}
-             style={styles.buttonContainer}>
-            <Button>
-              <Typo size={22} color={colors.neutral900} fontWeight={"600"}>
->>>>>>> 6163a122ef3268511a8c254e7e4007d395cfd283
                 Get Started
               </Typo>
             </Button>
@@ -133,7 +77,6 @@ const welcome = () => {
         </View>
       </View>
     </ScreenWrapper>
-<<<<<<< HEAD
   );
 };
 
@@ -172,42 +115,3 @@ const styles = StyleSheet.create({
     paddingHorizontal: spacingX._25,
   },
 });
-=======
-  )
-}
-
-export default welcome;
-const styles = StyleSheet.create({
-container: {
-flex: 1,
-justifyContent: "space-between",
-paddingTop: spacingY._7,
-},
-welcomeImage: {
-width: "100%",
-height: verticalScale (300),
-alignSelf: "center",
-marginTop: verticalScale(100),
-},
-loginButton: {
-alignSelf: "flex-end",
-marginRight: spacingX._20,
-},
-footer: {
-backgroundColor: colors.green,
-alignItems: "center",
-paddingTop: verticalScale(30),
-paddingBottom: verticalScale(45),
-gap: spacingY._20,
-shadowColor: "white",
-shadowOffset: { width: 8, height: -10},
-elevation: 10,
-shadowRadius: 25,
-shadowOpacity: 0.15,
-},
-buttonContainer: {
-width: "100%",
-paddingHorizontal: spacingX._25,
-},
-});
->>>>>>> 6163a122ef3268511a8c254e7e4007d395cfd283
